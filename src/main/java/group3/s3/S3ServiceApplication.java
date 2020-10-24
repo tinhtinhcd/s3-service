e8a6f80238b4dda79e3727cfcc4aa59c1cf2a188
tinhtinhcd <tinhtinhcd@gmail.com>
2020-10-24 15:57:03 -0500

    create project

---
+++ src/main/java/group3/s3/S3ServiceApplication.java
@@ -0,0 +1,13 @@
+package group3.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group3/s3/S3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package group3.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
