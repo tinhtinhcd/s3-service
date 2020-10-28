827dcbf77e16e9b749d3d499b7197787dd643070
tinhtinhcd <tinhtinhcd@gmail.com>
2020-10-27 19:21:20 -0500

    update eruka server

--- src/main/java/group3/s3/S3ServiceApplication.java
+++ src/main/java/group3/s3/S3ServiceApplication.java
@@ -2,7 +2,9 @@ package group3.s3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class S3ServiceApplication {
 	public static void main(String[] args) {
