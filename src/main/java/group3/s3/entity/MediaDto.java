e810022062fbf66202c10f5a412bd3254e606a0d
tinhtinhcd <tinhtinhcd@gmail.com>
2020-10-28 23:00:17 -0500

    update eruka

--- src/main/java/group3/s3/entity/MediaDto.java
+++ src/main/java/group3/s3/entity/MediaDto.java
@@ -2,12 +2,9 @@ package group3.s3.entity;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 @Data
 public class MediaDto {
 
     private String mediaType;
-    @NotNull
     private String url;
 }
